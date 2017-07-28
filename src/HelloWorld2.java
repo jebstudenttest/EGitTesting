fe383f0c0a260f9edbf56fbd9bdf20e551a2d19e
jburge <jburge@coloradocollege.edu>
2017-07-27 18:03:44 -0600

    Ok... lets see if Teammate 2 can get the files!

--- src/HelloWorld2.java
+++ src/HelloWorld2.java
@@ -1,8 +1,8 @@
-
+ 
 public class HelloWorld2 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world again!!!!");
+		System.out.println("Hello world again!!!! and again.. and again...");
 
 	}
 
---
+++ src/HelloWorld3.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld3 {
+
+	public HelloWorld3() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Getting git?");
+
+	}
+
+}
